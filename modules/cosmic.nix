9bb5c2220911d2b33d17450a04831970408c6410
KazuhiroIi <kazuhiro.ii@outlook.com>
2025-10-16 17:09:53 +0900

    feat: added cosmic settings

--- modules/cosmic.nix
+++ modules/cosmic.nix
@@ -1,4 +1,7 @@
 {
+  services.xserver.enable = true;
+  services.desktopManager.cosmic.enable = true;
+
   xdg.configFile."cosmic/com.system76.CosmicSettings.Shortcuts/v1/custom" = {
     source = ./.config/cosmic/custom-shortcuts;
   };
